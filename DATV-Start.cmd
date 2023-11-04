7739fa92ca5fa6f5bf646512a56e24e402d48fc8
Psynosaur <psynosaur@gmail.com>
2023-11-04 20:57:37 +0200

    bugs

--- DATV-Start.cmd
+++ DATV-Start.cmd
@@ -3,7 +3,7 @@ SET BASEDIR=%~dp0
 cd %BASEDIR%
 ECHO Please wait, programs starting...
 
-start "Node Red" .\DATV-Simple-NR.exe
+start "Node Red" .\node.exe .\npm\node_modules\node-red\red.js --settings .\.node-red\settings.js
 TIMEOUT /T 4 /NOBREAK
 start "" "http://127.0.0.1:1880/ui/"
 REM start "" "https://eshail.batc.org.uk/wb/"
