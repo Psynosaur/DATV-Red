6f9bab986dbfc86812fdde9b79bdcc78935b918c
Psynosaur <psynosaur@gmail.com>
2023-11-12 06:37:45 +0200

    V2.4.3    - set callsign from `pluto.json` in `device.json` when starting app      - we do not need to wait 5s    - VVC bitrate `800`      - Added `vvenc params`        - Framerate        - Size    - open browser restored    - Its getting harder to break things . . .

--- DATV-Start.cmd
+++ DATV-Start.cmd
@@ -2,7 +2,7 @@
 SET BASEDIR=%~dp0
 cd %BASEDIR%
 ECHO Please wait, programs starting...
-
+powershell .\setCallsign.ps1
 start "Node Red" .\npm\node-red.cmd --settings .\.node-red\settings.js --userDir .\.node-red\
 TIMEOUT /T 4 /NOBREAK
 start "" "http://127.0.0.1:1880/ui/"
