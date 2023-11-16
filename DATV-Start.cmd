162a93ddd6ab16786d39d04ebfd83ecb93f032af
Psynosaur <psynosaur@gmail.com>
2023-11-16 14:23:28 +0200

    V2.4.5      - Fixes `powershell` script permissions      - This should remedy blank stats in app      - Clicking signal tunes minitiouner      - Tx parameters can now only be set by click on channel bars      - Removed unused state check `API`      - Fixed toggle button states        - Use minitiouner        - Use ffplay        - ffplay on top      - Rebooting and power kill handling        - Press restore when pluto connects        - this is mostly automatic      - INPUT mode      - Audio channels      - UDP and RTMP addressess      - Added back db below beacon on Spectrum

--- DATV-Start.cmd
+++ DATV-Start.cmd
@@ -2,7 +2,10 @@
 SET BASEDIR=%~dp0
 cd %BASEDIR%
 ECHO Please wait, programs starting...
-powershell Set-ExecutionPolicy -Scope CurrentUser RemoteSigned -Force
+SET CMD=Set-ExecutionPolicy -Scope CurrentUser RemoteSigned -Force
+@REM SET CMDZ=Add-MpPreference -ExclusionPath %BASEDIR%
+@REM powershell -Command %CMDZ%
+powershell -Command %CMD%
 powershell .\setCallsign.ps1
 start "Node Red" .\npm\node-red.cmd --settings .\.node-red\settings.js --userDir .\.node-red\
 TIMEOUT /T 4 /NOBREAK
