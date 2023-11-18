5aa19b9096d70dee819918e18e23b2acaa9682de
Psynosaur <psynosaur@gmail.com>
2023-11-18 20:35:00 +0200

    patch

--- DATV-Start.cmd
+++ DATV-Start.cmd
@@ -2,11 +2,6 @@
 SET BASEDIR=%~dp0
 cd %BASEDIR%
 ECHO Please wait, programs starting...
-SET CMD=Set-ExecutionPolicy -Scope CurrentUser RemoteSigned -Force
-@REM SET CMDZ=Add-MpPreference -ExclusionPath %BASEDIR%
-@REM powershell -Command %CMDZ%
-powershell -Command %CMD%
-powershell .\setCallsign.ps1
 start "Node Red" .\npm\node-red.cmd --settings .\.node-red\settings.js --userDir .\.node-red\
 TIMEOUT /T 4 /NOBREAK
 start "" "http://127.0.0.1:1880/ui/"
--- setCallsign.ps1
+++
@@ -1,8 +0,0 @@
-$json = Get-Content -Path pluto.json -Raw | ConvertFrom-Json 
-$callsign = $json.payload
-
-$jsonDevice = Get-Content -Path device.json -Raw | ConvertFrom-Json 
-$jsonDevice.callsign = $callsign
-
-$saveBlob = $jsonDevice | ConvertTo-Json -Depth 9
-$saveBlob | Set-Content -Path device.json
