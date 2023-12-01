9cdc2b0febe0260923bc31fe6c574ecc27a93468
Ohan Smit <psynosaur@gmail.com>
2023-12-01 11:52:00 +0000

    Update DATV-Start.cmd

--- DATV-Start.cmd
+++ DATV-Start.cmd
@@ -1,9 +1,7 @@
 @ECHO OFF
 SET BASEDIR=%~dp0
 cd %BASEDIR%
-ECHO Please wait, programs starting...
+ECHO Starting DATV-Red...
 start "Node Red" .\npm\node-red.cmd --settings .\.node-red\settings.js --userDir .\.node-red\
-TIMEOUT /T 4 /NOBREAK
+TIMEOUT /T 5 /NOBREAK
 start "" "http://127.0.0.1:1880/ui/"
-REM start "" "https://eshail.batc.org.uk/wb/"
-ECHO All programs started!
