d9b475be56f9ec942b9716b55b064037962037b1
Psynosaur <psynosaur@gmail.com>
2023-12-15 11:11:41 +0200

    update script

--- scripts/update_firmware.cmd
+++ scripts/update_firmware.cmd
@@ -1,7 +1,9 @@
 @ECHO OFF
 SET BASEDIR=%~dp0
 SET pluto=%1
-IF [%1] == [] GOTO Done
+SET file=%2
+IF [%1] == [] GOTO NoIp
+IF [%2] == [] GOTO NoFile
 cd %BASEDIR%
 ECHO Copying firmware to pluto
 scp ./pluto.frm root@%pluto%:/root
@@ -9,6 +11,9 @@ ECHO Updating firmware
 ssh -o UserKnownHostsFile=\\.\NUL root@%pluto% ./update_frm_reboot.sh ./pluto.frm
 ECHO Rebooting pluto
 pause
-:Done
+:NoIp
 ECHO Please provide pluto network IP
+pause
+:NoFile
+ECHO Please provide path to firmware file
 pause
