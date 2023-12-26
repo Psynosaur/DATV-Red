2775498b4b04ee1e14dc915645bb029a0c4a6bf6
Psynosaur <psynosaur@gmail.com>
2023-12-26 08:33:08 +0200

    Test

--- scripts/set_NTP_pluto.cmd
+++ scripts/set_NTP_pluto.cmd
@@ -8,6 +8,7 @@ cd %BASEDIR%
 ECHO Setting up NTP server on pluto
 SET entry="'1s/^/server %ts%\n/'"
 @REM ECHO ssh -o UserKnownHostsFile=\\.\NUL root@%pluto% /etc/init.d/S49ntp stop ; sed -i %entry% /etc/ntp.conf ; ntpd -gq ; /etc/init.d/S49ntp start ; cat /etc/ntp.conf ; sleep 2 ; ntpq -c lpeer
+@REM ssh -o UserKnownHostsFile=\\.\NUL root@%pluto% sed -i %entry% /etc/ntp.conf ; /etc/init.d/S49ntp restart ; sleep 2 ; date
 ssh -o UserKnownHostsFile=\\.\NUL root@%pluto% /etc/init.d/S49ntp stop ; sed -i %entry% /etc/ntp.conf ; cat /etc/ntp.conf ; ntpd -gq ; /etc/init.d/S49ntp start ; sleep 2 ; ntpq -c lpeer ; date
 pause
 exit
