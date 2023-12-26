93f855fbf2e2ac8d1e6a729b2194af5c6325d15a
Psynosaur <psynosaur@gmail.com>
2023-12-26 17:15:25 +0200

    NTP script update

--- scripts/set_NTP_pluto.cmd
+++ scripts/set_NTP_pluto.cmd
@@ -3,20 +3,21 @@ SET BASEDIR=%~dp0
 SET pluto=%1
 SET ts=%2
 IF [%1] == [] GOTO NoIp
-IF [%2] == [] GOTO NoFile
+IF [%2] == [] GOTO NoTS
 cd %BASEDIR%
 ECHO Setting up NTP server on pluto
-SET entry="'1s/^/server %ts%\n/'"
-@REM ECHO ssh -o UserKnownHostsFile=\\.\NUL root@%pluto% /etc/init.d/S49ntp stop ; sed -i %entry% /etc/ntp.conf ; ntpd -gq ; /etc/init.d/S49ntp start ; cat /etc/ntp.conf ; sleep 2 ; ntpq -c lpeer
-@REM ssh -o UserKnownHostsFile=\\.\NUL root@%pluto% sed -i %entry% /etc/ntp.conf ; /etc/init.d/S49ntp restart ; sleep 2 ; date
-ssh -o UserKnownHostsFile=\\.\NUL root@%pluto% /etc/init.d/S49ntp stop ; sed -i %entry% /etc/ntp.conf ; cat /etc/ntp.conf ; ntpd -gq ; /etc/init.d/S49ntp start ; sleep 2 ; ntpq -c lpeer ; date
-pause
-exit
+GOTO Gogo
+
 :NoIp
-ECHO Please provide pluto network IP
-pause
-exit
-:NoFile
-ECHO  Please provide timeserver network IP
+@echo off
+set /p "pluto=Please provide pluto address: "
+:NoTS
+@echo off
+set /p "ts=Please enter time server address: "
+
+:Gogo
+SET entry="'1s/^/server %ts%\n/'"
+ECHO ssh -o UserKnownHostsFile=\\.\NUL root@%pluto% /etc/init.d/S49ntp stop ; sed -i %entry% /etc/ntp.conf ; ntpd -gq ; /etc/init.d/S49ntp start ; cat /etc/ntp.conf ; sleep 2 ; ntpq -c lpeer
+ssh -o UserKnownHostsFile=\\.\NUL root@%pluto% /etc/init.d/S49ntp stop ; sed -i %entry% /etc/ntp.conf ; cat /etc/ntp.conf ; ntpd -gq ; /etc/init.d/S49ntp start ; sleep 2 ; ntpq -c lpeer ; date
 pause
 exit
