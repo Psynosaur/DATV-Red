66daa2f66aeaa211665bf77138ff192b2d13601c
Psynosaur <psynosaur@gmail.com>
2023-12-30 06:14:12 +0200

    Firmware update fix

--- scripts/update_firmware.cmd
+++ scripts/update_firmware.cmd
@@ -5,18 +5,21 @@ SET file=%2
 IF [%1] == [] GOTO NoIp
 IF [%2] == [] GOTO NoFile
 cd %BASEDIR%
-ECHO Copying firmware to pluto
-scp -o UserKnownHostsFile=\\.\NUL ./pluto.frm root@%pluto%:/root
+:Gogo
+
+:NoIp
+@echo off
+set /p "pluto=Please provide pluto address: "
+:NoFile
+@echo off
+set /p "file=Please provide 'pluto.frm' path: "
+
+:Gogo
+scp -o UserKnownHostsFile=\\.\NUL %file% root@%pluto%:/root
 ECHO Updating firmware
 ssh -o UserKnownHostsFile=\\.\NUL root@%pluto% ./update_frm_reboot.sh ./pluto.frm
 ECHO Rebooting pluto
 pause
 exit
-:NoIp
-ECHO Please provide pluto network IP
-pause
-exit
-:NoFile
-ECHO Please provide path to firmware file
-pause
-exit
+
+
