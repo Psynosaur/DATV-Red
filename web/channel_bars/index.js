e86b0c26ea4721b4cae2492e94c03f7078041e79
Psynosaur <psynosaur@gmail.com>
2024-01-30 05:58:59 +0200

    - Removed mqtt mocked topics from sending every second - Prep frequency bar canvas component

---
+++ web/channel_bars/index.js
@@ -0,0 +1,54 @@
+Spectrum.prototype.drawChannels = function (ctx) {
+    ctx.fillStyle = "red";
+    const rolloff = 1.35 / 2.0;
+    const _start_freq = 491.5;
+
+    const width = ctx.canvas.width;
+    const height = ctx.canvas.height;
+
+    function draw_channel(center_frequency, bandwidth, line_height) {
+        if (typeof freq_info !== "undefined") {
+            if (freq_info.length === 44) freq_info = []; // hack to avoid continued push(). better to precompute all points and draw.
+            freq_info.push({
+                x1: (center_frequency - rolloff * bandwidth - _start_freq) * (width / 9),
+                x2: (center_frequency + rolloff * bandwidth - _start_freq) * (width / 9),
+                y: height * line_height,
+                center_frequency: center_frequency,
+                bandwidth: bandwidth,
+            });
+        }
+
+        ctx.fillRect((center_frequency - rolloff * bandwidth - _start_freq) * (width / 9), height * line_height, 2 * (rolloff * bandwidth) * (width / 9), 5);
+    }
+
+    /* 1MS */
+    for (let i = 493.25; i <= 496.25; i = i + 1.5) {
+        // draw_channel(f, 1.0, 7.475 / 8);
+        draw_channel(i, 1.0, 8.975 / 30);
+    }
+
+    /* 333Ks */
+    for (let j = 492.75; j <= 499.25; j = j + 0.5) {
+        draw_channel(j, 0.333, 8.15 / 30);
+    }
+
+    /* 125Ks */
+    for (let f = 492.75; f <= 499.25; f = f + 0.25) {
+        draw_channel(f, 0.125, 7.425 / 30);
+    }
+
+    /* Annotate Bands - Text */
+    ctx.font = "19px Arial";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    // ctx.fillText("A71A DATV Beacon", ((491.5) - _start_freq) * (width / 9), height - 45);
+    // ctx.fillText("10491.500", ((491.5) - _start_freq) * (width / 9), height - 28);
+    // ctx.fillText("(1.5MS/s QPSK, 4/5)", ((491.5) - _start_freq) * (width / 9), height - 12);
+    ctx.fillText("Click to tune wide & narrow channels", (494.75 - _start_freq) * (width / 9), 100);
+
+    /* ctx.fillText("", ((494.75) - _start_freq) * (canvasWidth / 15), canvasHeight - 6);
+    ctx.fillText("", ((494.75) - _start_freq) * (canvasWidth / 6.42), canvasHeight - 6); */
+
+    ctx.fillText("Narrow channels", (498.25 - _start_freq) * (width / 9), 100);
+    ctx.restore();
+};
