bf8f9d405db6ce535f9268985624faa2047a3df5
Psynosaur <psynosaur@gmail.com>
2024-01-30 05:57:25 +0200

    - Increase timeout

--- DATV-Start.cmd
+++ DATV-Start.cmd
@@ -3,5 +3,5 @@ SET BASEDIR=%~dp0
 cd %BASEDIR%
 ECHO Starting DATV-Red...
 start "Node Red" .\npm\node-red.cmd --settings .\.node-red\settings.js --userDir .\.node-red\
-TIMEOUT /T 2 /NOBREAK
+TIMEOUT /T 5 /NOBREAK
 start "" "http://127.0.0.1:1880/ui/"
