81936ceecb3e7fdd86c7bc405086934b168f330a
Psynosaur <psynosaur@gmail.com>
2024-02-01 12:22:14 +0200

    - Update color scheme and code refactoring in channel bars
    - Changed the default color for band and channel bars from a fixed hex color to a more grayscale RGB selection to enhance clarity.
    - Simplified variable names increasing code readability and standardized canvas conditions for readability and performance.
    - Additionally, added control loops to handle min/max data values and reset canvas variables as necessary.

--- web/channel_bars/index.js
+++ web/channel_bars/index.js
@@ -20,32 +20,37 @@ ctx.canvas.addEventListener("mouseout", function () {
     mouse_in_canvas = false;
 });
 let offset = 1910;
-let band_colour = "#c1c10b";
+let band_colour = "rgb(144,148,148)";
 let activeColor, activeXd1, activeYd, activeXd2;
-let activeColor_1_tx, activeXd1_1_tx, activeYd_1_tx, activeXd2_1_tx;
+let activeColor_tx, activeXd1_tx, activeYd_tx, activeXd2_tx;
 let downlink, uplink, canvasClickBW;
 let x1,x2,y1;
 
 /* Load vars from local storage */
 if (typeof Storage !== "undefined") {
     storageSupport = true;
-    if (localStorage.activeColor_1_tx) {
-        activeColor_1_tx = localStorage.activeColor_1_tx;
+    if (localStorage.activeColor_tx) {
+        activeColor_tx = localStorage.activeColor_tx;
     }
-    if (localStorage.activeXd1_1_tx) {
-        activeXd1_1_tx = localStorage.activeXd1_1_tx;
+    if (localStorage.activeXd1_tx) {
+        activeXd1_tx = localStorage.activeXd1_tx;
     }
-    if (localStorage.activeYd_1_tx) {
-        activeYd_1_tx = localStorage.activeYd_1_tx;
+    if (localStorage.activeYd_tx) {
+        activeYd_tx = localStorage.activeYd_tx;
     }
-    if (localStorage.activeXd2_1_tx) {
-        activeXd2_1_tx = localStorage.activeXd2_1_tx;
+    if (localStorage.activeXd2_tx) {
+        activeXd2_tx = localStorage.activeXd2_tx;
     }
 }
 
 function on_canvas_click(ev) {
+    console.log(mouse_x)
+    console.log(mouse_y)
+    console.log(`${uplink}:${canvasClickBW}`)
     let u = uplink
     let sr = canvasClickBW
+    if(canvasClickBW === undefined || uplink === undefined) return
+
     const url = localPage ? 'http://127.0.0.1:1880' : '';
     /* we clicked on a channel bar... */
 
@@ -58,7 +63,6 @@ function on_canvas_click(ev) {
         canvasClickBW = 0.25;
     }
 
-
     fetch(
         `${url}/setTx?` +
         new URLSearchParams({
@@ -67,15 +71,15 @@ function on_canvas_click(ev) {
             SR: sr,
         })
     );
-    activeColor_1_tx = activeColor;
-    activeXd1_1_tx = activeXd1;
-    activeYd_1_tx = activeYd;
-    activeXd2_1_tx = activeXd2 + 1;
+    activeColor_tx = activeColor;
+    activeXd1_tx = activeXd1;
+    activeYd_tx = activeYd;
+    activeXd2_tx = activeXd2 + 1;
     if (storageSupport) {
-        localStorage.activeColor_1_tx = activeColor_1_tx;
-        localStorage.activeXd1_1_tx = activeXd1_1_tx;
-        localStorage.activeYd_1_tx = activeYd_1_tx;
-        localStorage.activeXd2_1_tx = activeXd2_1_tx;
+        localStorage.activeColor_tx = activeColor_tx;
+        localStorage.activeXd1_tx = activeXd1_tx;
+        localStorage.activeYd_tx = activeYd_tx;
+        localStorage.activeXd2_tx = activeXd2_tx;
     }
 }
 
@@ -85,7 +89,7 @@ function drawChannels(ctx) {
     let freq_info = [];
     resize(canvas)
 
-    ctx.fillStyle = "#FF0000";
+    ctx.fillStyle = band_colour;
     let offset = 1910.5;
     const rolloff = 1.35 / 2.0;
     const _start_freq = 491.5;
@@ -107,24 +111,24 @@ function drawChannels(ctx) {
         ctx.fillRect((center_frequency - rolloff * bandwidth - _start_freq) * (width / 9), height * line_height, 2 * (rolloff * bandwidth) * (width / 9), 6);
     }
 
-    ctx.fillStyle = "#FF0000";
+    ctx.fillStyle = band_colour;
     /* 1MS */
     for (let i = 493.25; i <= 496.25; i = i + 1.5) {
         // draw_channel(f, 1.0, 7.475 / 8);
 
-        draw_channel(i, 1.0, 8.975 / 20);
+        draw_channel(i, 1.0, 0.6);
     }
 
     /* 333Ks */
     for (let j = 492.75; j <= 499.25; j = j + 0.5) {
 
-        draw_channel(j, 0.333, 7.2 / 20);
+        draw_channel(j, 0.333, 0.5);
     }
 
     /* 125Ks */
     for (let f = 492.75; f <= 499.25; f = f + 0.25) {
 
-        draw_channel(f, 0.125, 5.425 / 20);
+        draw_channel(f, 0.125, 0.4);
     }
     if(mouse_in_canvas){
         // Mouse hover over rectangle
@@ -134,12 +138,12 @@ function drawChannels(ctx) {
 
     ctx.fillStyle = invertColor(band_colour);
     ctx.font = "bold 19px Arial";
-    ctx.fillText("TX", activeXd1_1_tx - 20, activeYd_1_tx);
+    ctx.fillText("TX", activeXd1_tx - 20, activeYd_tx);
 
     ctx.fillRect(
-        activeXd1_1_tx,
-        activeYd_1_tx,
-        activeXd2_1_tx - activeXd1_1_tx,
+        activeXd1_tx,
+        activeYd_tx,
+        activeXd2_tx - activeXd1_tx,
         7
     );
     /* Annotate Bands - Text */
@@ -149,12 +153,12 @@ function drawChannels(ctx) {
     // ctx.fillText("A71A DATV Beacon", ((491.5) - _start_freq) * (width / 9), height - 45);
     // ctx.fillText("10491.500", ((491.5) - _start_freq) * (width / 9), height - 28);
     // ctx.fillText("(1.5MS/s QPSK, 4/5)", ((491.5) - _start_freq) * (width / 9), height - 12);
-    ctx.fillText("Click to tune wide & narrow channels", (494.75 - _start_freq) * (width / 9), 75);
+    ctx.fillText("Click to tune wide & narrow channels", (494.75 - _start_freq) * (width / 9), 90);
 
     /* ctx.fillText("", ((494.75) - _start_freq) * (canvasWidth / 15), canvasHeight - 6);
     ctx.fillText("", ((494.75) - _start_freq) * (canvasWidth / 6.42), canvasHeight - 6); */
 
-    ctx.fillText("Narrow channels", (498.25 - _start_freq) * (width / 9), 75);
+    ctx.fillText("Narrow channels", (498.25 - _start_freq) * (width / 9), 90);
     if (storageSupport) {
         localStorage.freq_info = JSON.stringify(freq_info)
     }
@@ -178,15 +182,46 @@ function resize(canvas) {
     //     this.axes.height = this.spectrumHeight;
     // }
 }
+function getMinY(data) {
+    return data.reduce((min, p) => p.y < min ? p.y : min, data[0].y);
+}
+function getMaxY(data) {
+    return data.reduce((max, p) => p.y > max ? p.y : max, data[0].y);
+}
+function getMinX(data) {
+    return data.reduce((min, p) => p.x < min ? p.x : min, data[0].x);
+}
+function resetCanvasVariables() {
+    canvasClickBW = undefined;
+    uplink = undefined;
+}
 
 function render_frequency_info(mouse_x, mouse_y) {
     let freq = [];
     if (typeof Storage !== "undefined") {
         freq = JSON.parse(localStorage.freq_info);
     }
+    let minX = getMinX(freq);
+    let minY = getMinY(freq);
+    let maxY = getMaxY(freq);
     let display_triggered = false;
-
-    if (mouse_y > 0) {
+    if (mouse_x < minX) {
+        resetCanvasVariables();
+        return
+    }
+    if (mouse_y < minY) {
+        resetCanvasVariables();
+        return
+    }
+    if (mouse_x > freq[43].x2) {
+        resetCanvasVariables();
+        return
+    }
+    if (mouse_y > freq[0].y + 6) {
+        resetCanvasVariables();
+        return
+    }
+    if (mouse_y > minY ) {
         for (let i = 0; i < freq.length; i++) {
             let xd1 = freq[i].x1;
             let xd2 = freq[i].x2;
@@ -195,7 +230,7 @@ function render_frequency_info(mouse_x, mouse_y) {
                 mouse_x > xd1 - 1 &&
                 mouse_x < xd2 + 1 &&
                 mouse_y > yd - 1
-                // && mouse_y < yd + 6
+                && mouse_y < yd + 6
             ) {
                 downlink = 10000.0 + freq[i].center_frequency - offset;
                 uplink = freq[i].center_frequency;
@@ -237,11 +272,6 @@ function render_frequency_info(mouse_x, mouse_y) {
     }
 }
 
-// function resetCanvasVariables() {
-//     canvasClickBW = undefined;
-//     uplink = undefined;
-//     busy = false;
-// }
 
 function invertColor(hexTripletColor) {
     let color = hexTripletColor;
