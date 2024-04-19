e07922f2a6b2f3b2c2eb628aa7673b545d5eaffc
Psynosaur <psynosaur@gmail.com>
2024-04-19 06:24:17 +0200

    - Reset BBFrame graph - Adding presets box, work in progress - DATV-Red exec again...   - https://www.virustotal.com/gui/file/a458618c3326d4f9c7b507d70f84410836e5a6731bacf9f69d4c97da88f567e6/details

---
+++ exe_source/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace DATV_RED
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string strCmdText;
+            strCmdText= @"/C start ""DATV-RED"" .\npm\node-red.cmd --userDir .\.node-red\";
+            System.Diagnostics.Process.Start("CMD.exe",strCmdText);
+        }
+    }
+}
--- red.cmd
+++
@@ -1,1 +0,0 @@
-start "Node Red" .\npm\node-red.cmd --userDir .\.node-red\
